3e6a539b4e4045134d2e5d4e23347dba6caa361c
Sotiris Karampatakis <sotiris.karampatakis@semantic-web.com>
2025-02-04 19:15:00 +0100

    Add all args constructor, changed visibility, match checkstyle

--- utils/catalog-parser/src/main/java/de/sovity/edc/utils/catalog/CatalogMetadataSender.java
+++ utils/catalog-parser/src/main/java/de/sovity/edc/utils/catalog/CatalogMetadataSender.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright (c) 2025 Graphwise.ai
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Apache License, Version 2.0 which is available at
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Contributors:
+ *       Graphwise.ai - init
+ *
+ */
+
 package de.sovity.edc.utils.catalog;
 
 import lombok.RequiredArgsConstructor;
@@ -20,11 +34,18 @@ import static jakarta.ws.rs.core.Response.Status.ACCEPTED;
 @RequiredArgsConstructor
 public class CatalogMetadataSender {
 
-    private static final HttpClient HTTP_CLIENT = HttpClient.newBuilder().connectTimeout(Duration.ofSeconds(60)).build();
-    private static final int MAX_TRIALS = 3;
-    private static final String METADATA_INGESTER_ENDPOINT = "http://metadata-ingester:8080/ingest";
+    private HttpClient httpClient;
+    private int maxTrials;
+    private String metadataIngesterEndpoint;
 
     private final Monitor monitor;
+
+    public CatalogMetadataSender(int maxTrials, int connectionTimeout, String metadataIngesterEndpoint, Monitor monitor) {
+        this.httpClient = HttpClient.newBuilder().connectTimeout(Duration.ofSeconds(connectionTimeout)).build();
+        this.maxTrials = maxTrials;
+        this.metadataIngesterEndpoint = metadataIngesterEndpoint;
+        this.monitor = monitor;
+    }
 
     public void sendMetadataForIngestion(String metadata) {
         int statusCode = 0;
@@ -32,17 +53,17 @@ public class CatalogMetadataSender {
         do {
             trialNumber++;
             try {
-                HttpRequest request = HttpRequest.newBuilder(new URI(METADATA_INGESTER_ENDPOINT))
+                HttpRequest request = HttpRequest.newBuilder(new URI(metadataIngesterEndpoint))
                     .header(CONTENT_TYPE, APPLICATION_JSON)
                     .timeout(Duration.of(60, ChronoUnit.SECONDS))
                     .POST(HttpRequest.BodyPublishers.ofString(metadata))
                     .build();
-                CompletableFuture<Integer> responseCode = HTTP_CLIENT.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                CompletableFuture<Integer> responseCode = httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString())
                     .thenApply(HttpResponse::statusCode);
                 statusCode = responseCode.get();
             } catch (InterruptedException | ExecutionException | URISyntaxException e) {
                 monitor.severe("Could not successfully send the metadata to the Metadata Ingester!", e);
             }
-        } while ((statusCode != ACCEPTED.getStatusCode()) && trialNumber <= MAX_TRIALS);
+        } while ((statusCode != ACCEPTED.getStatusCode()) && trialNumber <= maxTrials);
     }
 }
